090329d01c3e79a1366f968704b09fc07fbe81a8
Harllem Nascimento <harllemnascimento@gmail.com>
2023-08-13 17:36:52 -0300

    Projeto hr-oauth criado

---
+++ hr-oauth/src/test/java/com/tiduswr/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiduswr.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
