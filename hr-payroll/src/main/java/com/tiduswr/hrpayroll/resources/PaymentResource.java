f9bd8481cb5865783768e5299f0ba7de18777b6d
Harllem Nascimento <harllemnascimento@gmail.com>
2023-08-13 14:14:14 -0300

    Configurando Servidor de Configuração

--- hr-payroll/src/main/java/com/tiduswr/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/tiduswr/hrpayroll/resources/PaymentResource.java
@@ -1,7 +1,6 @@
 package com.tiduswr.hrpayroll.resources;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
